e561a46c4fb06670cb1825a14b6180b83fd6e8ff
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-12-18 14:03:57 -0400

    structure-inital-calendar

--- calendar-app/src/components/calendar/CalendarScreen.jsx
+++ calendar-app/src/components/calendar/CalendarScreen.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Navbar } from '../ui/Navbar'
 
+
+const localizer = momentLocalizer(moment)
+
+const events = [{
+    title:'Compleaños de marcos',
+    start: moment().toDate(),
+    end : moment().add(2, 'hours').toDate(),
+    bgColor: '#fafafa'
+}]
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className='calendar-screen'>
             <Navbar/>
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            />
         </div>
     )
 }
